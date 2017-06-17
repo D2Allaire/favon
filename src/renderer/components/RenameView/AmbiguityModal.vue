79248711e2c1b0e56315b6df9247b1e18233f901
Elisha Witte <elishawitte@gmail.com>
2017-06-17 13:18:21 +0200

    Fix ambiguity check not working for movies anymore

--- src/renderer/components/RenameView/AmbiguityModal.vue
+++ src/renderer/components/RenameView/AmbiguityModal.vue
@@ -79,46 +79,63 @@
         const original = this.ambiguousFiles[this.currentIndex].original;
         const selected = this.ambiguousFiles[this.currentIndex].results[this.selected];
         if (original instanceof Movie) {
-          const movie = new Movie(
-            original.path,
-            original.name,
-            original.format,
-            FileMatcher.cleanString(selected.title),
-            Number(selected.release_date.split('-')[0]),
-          );
-          movie.renamed = original.renamed;
-          this.resolvedFiles.push(movie);
-          if ((this.currentIndex + 1) < this.ambiguousFiles.length) {
-            for (let i = this.currentIndex + 1; i < this.ambiguousFiles.length; i++) {
-              if (original.name === this.ambiguousFiles[i].original.name) {
-                const nextOriginal = this.ambiguousFiles[i].original;
-                const nextMovie = new Movie(
-                  nextOriginal.path,
-                  nextOriginal.name,
-                  nextOriginal.format,
-                  FileMatcher.cleanString(selected.title),
-                  Number(selected.release_date.split('-')[0]),
-                );
-                nextMovie.renamed = nextOriginal.renamed;
-                this.resolvedFiles.push(nextMovie);
-                this.$store.commit('REMOVE_AMBIGUOUS', i);
-              }
+          this.resolveMovie(original, selected);
+        }
+        if (original instanceof Series) {
+          this.resolveSeries(original, selected);
+        }
+      },
+      resolveMovie(original, selected) {
+        const movie = new Movie(
+          original.path,
+          original.name,
+          original.format,
+          FileMatcher.cleanString(selected.title),
+          Number(selected.release_date.split('-')[0]),
+        );
+        movie.renamed = original.renamed;
+        this.resolvedFiles.push(movie);
+        // If there are more ambigious files to resolve...
+        if ((this.currentIndex + 1) < this.ambiguousFiles.length) {
+          // ...loop through each one and check if they are already resolved
+          // (e.g. don't show ambiguity modal twice for MKV and SRT)
+          for (let i = this.currentIndex + 1; i < this.ambiguousFiles.length; i++) {
+            if (original.name === this.ambiguousFiles[i].original.name) {
+              const nextOriginal = this.ambiguousFiles[i].original;
+              const nextMovie = new Movie(
+                nextOriginal.path,
+                nextOriginal.name,
+                nextOriginal.format,
+                FileMatcher.cleanString(selected.title),
+                Number(selected.release_date.split('-')[0]),
+              );
+              nextMovie.renamed = nextOriginal.renamed;
+              this.resolvedFiles.push(nextMovie);
+              this.$store.commit('REMOVE_AMBIGUOUS', i);
             }
           }
         }
-        if (original instanceof Series) {
-          const series = new Series(...original.getProperties());
-          series.matchedShow = selected.seriesName;
-          series.renamed = original.renamed;
-          series.id = selected.id;
-
-          EventBus.$emit('series-ambiguity-cleared', selected);
-
-          if ((this.currentIndex + 1) >= this.ambiguousFiles.length) {
-            this.$store.commit('EMPTY_AMBIGUOUS');
-          } else {
-            this.currentIndex++;
+        if ((this.currentIndex + 1) >= this.ambiguousFiles.length) {
+          for (let i = 0; i < this.resolvedFiles.length; i++) {
+            this.$store.commit('ADD_FILE', this.resolvedFiles[i]);
           }
+          this.$store.commit('EMPTY_AMBIGUOUS');
+        } else {
+          this.currentIndex++;
+        }
+      },
+      resolveSeries(original, selected) {
+        const series = new Series(...original.getProperties());
+        series.matchedShow = selected.seriesName;
+        series.renamed = original.renamed;
+        series.id = selected.id;
+
+        EventBus.$emit('series-ambiguity-cleared', selected);
+
+        if ((this.currentIndex + 1) >= this.ambiguousFiles.length) {
+          this.$store.commit('EMPTY_AMBIGUOUS');
+        } else {
+          this.currentIndex++;
         }
       },
     },
